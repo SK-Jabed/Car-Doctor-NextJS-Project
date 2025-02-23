b32396db2a9df3b6f143987b8d1dac78212a489a
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-23 22:37:59 +0600

    New Routes Added and Basic Routes Setup Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,9 @@
 import Image from "next/image";
 
-
-
-
-
-
-
-
-
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -21,15 +11,11 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            
             Deploy now
           </a>
-         
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
